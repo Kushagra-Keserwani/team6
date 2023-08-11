2355913975bbd34a24d5fb7869d973b35ab53186
premasai09 <premasai09@gmail.com>
2023-08-11 15:26:32 +0530

    FormsDone

--- src/Pages/AddCustomer/AddCustomer.js
+++ src/Pages/AddCustomer/AddCustomer.js
@@ -1,24 +1,45 @@
 import React from "react";
+import './AddCustomer.css';
 
 function AddCustomer() {
   return (
     <div>
-      <h3>Add Customer details</h3>
-      <form>
-        <div class="mb-3">
-          <label for="name" class="form-label">Name</label>
-          <input type="text" class="form-control" id="name" ></input>
+      <body>
+        <div class="container">
+          <h2>Add Customer Details</h2>
+          <form action="#" method="post">
+            <div class="form-group">
+              <label for="name">Name:</label>
+              <input type="text" class="form-control" id="name" ></input>
+            </div>
+            <div class="form-group">
+              <label for="email">Email:</label>
+              <input type="email" class="form-control" id="email" ></input>
+            </div>
+            <div class="form-group">
+              <label for="mobile">Mobile:</label>
+              <input type="text" class="form-control" id="mobile" ></input>
+            </div>
+            <div class="form-group">
+              <label for="address">Address:</label>
+              <textarea id="address" name="address" rows="4"></textarea>
+            </div>
+            <div class="form-group">
+              <label for="accno">Account Number:</label>
+              <input type="text" class="form-control" id="accno" ></input>
+            </div>
+            <div class="form-group">
+              <label for="acctype">Account Type:</label>
+              <input type="text" class="form-control" id="acctype" ></input>
+            </div>
+            <div class="form-group">
+              <label for="bal">Balance:</label>
+              <input type="text" class="form-control" id="bal" ></input>
+            </div>
+            <button type="submit" class="btn btn-primary">Add</button>
+          </form>
         </div>
-        <div class="mb-3">
-          <label for="accountNo" class="form-label">Account Number</label>
-          <input type="text" class="form-control" id="accountNo" ></input>
-        </div>
-        <div class="mb-3">
-          <label for="balance" class="form-label">Balance</label>
-          <input type="number" class="form-control" id="balance"></input>
-        </div>
-        <button type="submit" class="btn btn-primary">Add</button>
-      </form>
+      </body>
     </div>
   );
 }
