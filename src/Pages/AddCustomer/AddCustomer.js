81e1f79d1264e0e0aa85c9c5edd0d8caed2e8259
sinhashubham2002 <connectwithshubham.sinha@gmail.com>
2023-08-16 15:30:37 +0530

    view customer

--- src/Pages/AddCustomer/AddCustomer.js
+++ src/Pages/AddCustomer/AddCustomer.js
@@ -1,44 +1,260 @@
 import React from "react";
 import './AddCustomer.css';
-import SideMenuBar from "./../../components/sidebar/index"
+import axios from "axios";
+import {useEffect, useState} from "react";
+import SideMenuBar from "./../../components/sidebar/index";
 
 function AddCustomer() {
+
+  const [accountnum, setAccountNum] = useState("");
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [contact, setContact] = useState("");
+  const [cardnumber, setCardNumber] = useState("");
+  const [pinnum, setPinNum] = useState("");
+  const [city, setCity] = useState("");
+  const [accounttype, setAccountType] = useState("");
+  const [balance, setBalance] = useState("");
+  const [customers, setUsers] = useState([]);
+  
+
+  useEffect(() => {
+    (async () => await Load())();
+}, []);
+
+async function Load(){
+    const result = await axios.get("https://localhost:7254/api/customer/Getcustomer");
+    setUsers(result.data);
+    console.log(result.data);
+}
+
+async function save(event){
+    event.preventDefault();
+    try{
+        await axios.post("https://localhost:7254/api/customer/Addcustomer",{
+            name: name,
+            address: address,
+            email: email,
+            contact: contact,
+            cardnumber: cardnumber,
+            pinnum : pinnum,
+            city:city,
+            accounttype:accounttype,
+            balance: balance,
+        });
+        alert("Customer Added Successfully.");
+        setAccountNum("");
+        setName("");
+        setAddress("");
+        setEmail("");
+        setContact("");
+        setCardNumber("");
+        setPinNum("");
+        setCity("");
+        setAccountType("");
+        setBalance("");
+
+        Load();
+    } catch(err){
+        alert(err);
+    }
+}
+
+async function editCustomer(customers){
+    setName(customers.name);
+    setAddress(customers.address);
+    setEmail(customers.email);
+    setContact(customers.contact);
+    setCardNumber(customers.cardnumber);
+    setPinNum(customers.pinnum);
+    setCity(customers.city);
+    setAccountType(customers.accounttype);
+    setBalance(customers.balance);
+    setAccountNum(customers.accountnum);
+}
+
+async function deleteCustomer(accountnum)
+{
+    await axios.delete("https://localhost:7254/api/customer/Deletecustomer/" + accountnum);
+    alert("Customer deleted successfully.");
+    setAccountNum("");
+    setName("");
+    setAddress("");
+    setEmail("");
+    setContact("");
+    setCardNumber("");
+    setPinNum("");
+    setCity("");
+    setAccountType("");
+    setBalance("");
+    Load();
+}
+
+async function update(event){
+    event.preventDefault();
+    try{
+        await axios.patch("https://localhost:7254/api/customer/Updatecustomer/" +customers.find((u) => u.accountnum=== accountnum).accountnum || accountnum,
+        {
+            accountnum: accountnum,
+            name: name,
+            address: address,
+            email: email,
+            contact: contact,
+            cardnumber: cardnumber,
+            pinnum : pinnum,
+            city: city,
+            accounttype : accounttype,
+            balance: balance,
+        }
+        );
+        alert("Customer Updated.");
+        setAccountNum("");
+        setName("");
+        setAddress("");
+        setEmail("");
+        setContact("");
+        setCardNumber("");
+        setPinNum("");
+        setCity("");
+        setAccountType("");
+        setBalance("");
+
+        Load();
+
+    } catch(err){
+        alert(err);
+    }
+  }
   return (
     <div>
     <SideMenuBar></SideMenuBar>
       <body>
         <div className="container1">
           <h2>Add Customer Details</h2>
-          <form action="#" method="post">
+          <form>
+          <div className="form-group">
+          <input
+                    type="text"
+                    className="form-control1"
+                    id="accountnum"
+                    hidden value={accountnum}
+                    onChange={(event) => {
+                        setAccountNum(event.target.value);
+                    }}
+                    />
+            </div>
+
             <div className="form-group">
               <label for="name">Name:</label>
-              <input type="text" className="form-control1" id="name" ></input>
+              <input
+                        type="text"
+                        className="form-control1"
+                        id="name"
+                        value ={name}
+                        onChange={(event) => {
+                            setName(event.target.value);}}
+                     />
             </div>
             <div className="form-group">
               <label for="email">Email:</label>
-              <input type="email" className="form-control1" id="email" ></input>
+              <input 
+                        type="email"
+                        className="form-control1"
+                        id="email"
+                        value={email}
+                        onChange={(event) => {
+                            setEmail(event.target.value);
+                        }}
+                        />
             </div>
             <div className="form-group">
               <label for="mobile">Mobile:</label>
-              <input type="text" className="form-control1" id="mobile" ></input>
+              <input 
+                        type="text"
+                        className="form-control1"
+                        id="mobile"
+                        value={contact}
+                        onChange={(event) => {
+                            setContact(event.target.value);
+                        }}
+                        />
             </div>
             <div className="form-group">
               <label for="address">Address:</label>
-              <textarea id="address" name="address" rows="4"></textarea>
+              <textarea 
+                        type="text"
+                        className="form-control1"
+                        id="address"
+                        rows="4"
+                        value={address}
+                        onChange={(event) => {
+                            setAddress(event.target.value);
+                        }}
+                        ></textarea>              
             </div>
             <div className="form-group">
-              <label for="accno">Account Number:</label>
-              <input type="text" className="form-control1" id="accno" ></input>
+              <label for="cardnumber">Card Number:</label>
+              <input 
+                        type="text"
+                        className="form-control1"
+                        id="cardnumber"
+                        value={cardnumber}
+                        onChange={(event) => {
+                            setCardNumber(event.target.value);
+                        }}
+                        />
             </div>
             <div className="form-group">
-              <label for="acctype">Account Type:</label>
-              <input type="text" className="form-control1" id="acctype" ></input>
-            </div>
-            <div className="form-group">
-              <label for="bal">Balance:</label>
-              <input type="text" className="form-control1" id="bal" ></input>
-            </div>
-            <button type="submit" className="btn btn-primary">Add</button>
+                        <label for="pinnum">Pin Number:</label>
+                        <input
+                        type="text"
+                        className="form-control1"
+                        id="pinnum"
+                        value={pinnum}
+                        onChange={(event) => {
+                            setPinNum(event.target.value);
+                        }}
+                         />
+                    </div>
+
+                    <div className="form-group">
+                        <label for="city">City:</label>
+                        <input 
+                        type="text"
+                        className="form-control1"
+                        id="city"
+                        value={city}
+                        onChange={(event) => {
+                            setCity(event.target.value);
+                        }}
+                        />
+                    </div>            
+                    <div className="form-group">
+                        <label for="accounttype">Account Type:</label>
+                        <input 
+                        type="text"
+                        className="form-control1"
+                        id="accounttype"
+                        value={accounttype}
+                        onChange={(event) => {
+                            setAccountType(event.target.value);
+                        }}
+                        />
+                    </div>
+                    <div className="form-group">
+                            <label for="balance">Balance:</label>
+                            <input
+                            type="text"
+                            className="form-control1"
+                            id="balance"
+                            value={balance}
+                            onChange={(event) =>{
+                                setBalance(event.target.value);
+                            }}
+                            />
+                    </div>
+            <button className="btn btn-primary" onClick={save}>Add</button>
           </form>
         </div>
       </body>
