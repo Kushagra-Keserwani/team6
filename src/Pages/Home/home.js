49c4763a0cbf1a5c8172921c137e254c3409c5cc
Kushagra-Keserwani <kushagra_bt2k19@dtu.ac.in>
2023-08-23 15:42:33 +0530

    home page done

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,21 +1,112 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SideMenuBar from "./../../components/sidebar/index"
+import transactionImage from "./transaction.jpg"
+import balanceImage from "./balance check.png";
+import viewTransaction from "./viewTransaction.jpg";
+import viewCustomer from "./viewCustomer.png";
+import editCustomer from "./editCustomer.jpg";
+import addCustomer from "./addCustomer.jpg"
 import Header from "../../components/Header/Header";
 import "./home.css"
+import { Card, Row, Col } from 'react-bootstrap'; // Import Card component from react-bootstrap
+
+  
+
 function Home() {
     return (
         <div className="home">
             <SideMenuBar></SideMenuBar>
             <div className="leftSpace">
             <div className="container text-left mt-5">
-                <h3 class="display-4">Welcome to DOTBank, where your financial dreams come to life!</h3>
-                <p class="lead">
-                    At DOTBank, we understand that each individual's financial journey is unique.
-                    That's why we are here to support you every step of the way, providing a range
-                    of services and solutions tailored to your needs.
-                </p>
+                <h4 class="display-6">Welcome to DOTBank, where your financial dreams come to life!</h4>
+
+                <div>
+                <Row >
+                   
+    <Col  md = {4} sm = {6} xs = {12} className="col">
+    <Card style = {{ width: '18rem' }} >
+    <Card.Img variant = "top" className = "card-image" src               = {transactionImage} />
+    <Card.Body className = "options">
+    <Link      to        = "/addTransactions">
+    <button    className = 'btn btn-danger btn-lg btn-block cardButton'>
+              Add  Transactions
+            </button>
+        </Link>
+      </Card.Body>
+    </Card>
+    </Col>
+    <Col      md = {4} sm = {6} xs = {12} className="col">
+    <Card     style   = {{ width: '18rem' }} >
+    <Card.Img variant = "top" className = "card-image" src               = {balanceImage} />
+      <Card.Body className="options">
+        <Link   to        = "/balancecheck">
+        <button className = 'btn btn-danger btn-lg btn-block cardButton'>
+              Check Balance
+            </button>
+        </Link>
+      </Card.Body>
+    </Card>
+    </Col>
+    <Col      md = {4} sm = {6} xs = {12} className="col">
+    <Card     style   = {{ width: '18rem' }} >
+    <Card.Img variant = "top" className = "card-image" src               = {viewTransaction} />
+      <Card.Body className="options">
+        <Link   to        = "/transactions">
+        <button className = 'btn btn-danger btn-lg btn-block cardButton'>
+               Last Transactions
+            </button>
+        </Link>
+      </Card.Body>
+    </Card>
+    </Col>
+    
+    </Row>
+    <Row className="row">
+    <Col      md = {4} sm = {6} xs = {12} className="col">
+    <Card     style   = {{ width: '18rem' }} >
+    <Card.Img variant = "top" className = "card-image" src               = {addCustomer} />
+      <Card.Body className="options">
+        <Link   to        = "/addCustomer">
+        <button className = 'btn btn-danger btn-lg btn-block cardButton'>
+              Add  Customer
+            </button>
+        </Link>
+      </Card.Body>
+    </Card>
+    </Col>
+    <Col      md = {4} sm = {6} xs = {12} className="col">
+    <Card     style   = {{ width: '18rem' }} >
+    <Card.Img variant = "top" className = "card-image" src               = {viewCustomer} />
+      <Card.Body className="options">
+        <Link   to        = "/viewCustomer">
+        <button className = 'btn btn-danger btn-lg btn-block cardButton'>
+              Customer Details
+            </button>
+        </Link>
+      </Card.Body>
+    </Card>
+    </Col>
+    <Col      md = {4} sm = {6} xs = {12} className="col">
+    <Card     style   = {{ width: '18rem' }} >
+    <Card.Img variant = "top" className = "card-image" src               = {editCustomer} />
+      <Card.Body className="options">
+        <Link   to        = "/editCustomer">
+        <button className = 'btn btn-danger btn-lg btn-block cardButton'>
+              Edit Customer
+            </button>
+        </Link>
+      </Card.Body>
+    </Card>
+    </Col>
+
+    </Row>
+    </div>
+
             </div>
+           
+    
+
             </div>
 
         </div>
