3eed10f6171c5c8040e9a4f0329080b94599c382
Kushagra-Keserwani <kushagra_bt2k19@dtu.ac.in>
2023-08-28 14:28:26 +0530

    Toater changes + user icon

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -4,7 +4,7 @@ import React, { useRef, useState } from "react"
 import Header from "../../components/Header/Header";
 // import { useAuth } from "../../contexts/AuthContext"
 import { Link, useNavigate } from "react-router-dom"
-import { toast } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 
 function Login() {
     const emailRef = useRef()
@@ -12,8 +12,8 @@ function Login() {
     const passwordConfirmRef = useRef()
     const loginEmailRef = useRef()
     const loginPasswordRef = useRef()
-    const [role,setRole] = useState("Admin")
-    const [accNo,setaccNo] = useState("0")
+    const [role, setRole] = useState("Admin")
+    const [accNo, setaccNo] = useState("0")
     // const { signup, login } = useAuth()
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
@@ -25,17 +25,17 @@ function Login() {
             toast.warn("Passwords do not match.")
             return;
         }
-            loginEmailRef.current=emailRef.current;
-            loginPasswordRef.current=passwordRef.current;
+        loginEmailRef.current = emailRef.current;
+        loginPasswordRef.current = passwordRef.current;
         try {
             setLoading(true)
             console.log(role.current);
-            await axios.post("https://localhost:7254/api/Auth/register",{
+            await axios.post("https://localhost:7254/api/Auth/register", {
                 username: emailRef.current.value,
                 password: passwordRef.current.value,
-                role:role,
-                accNo:accNo,
-            }).then((response)=>{
+                role: role,
+                accNo: accNo,
+            }).then((response) => {
                 console.log(response.data);
                 toast.success("Registered Successfully.");
             });
@@ -43,7 +43,8 @@ function Login() {
             // await signup(emailRef.current.value, passwordRef.current.value)
             //navigate("/")
         } catch (err) {
-            alert(err)
+            toast.error("Failed to create an account.")
+            // alert(err)
         }
 
         setLoading(false)
@@ -54,29 +55,30 @@ function Login() {
 
         try {
             setLoading(true)
-            await axios.post("https://localhost:7254/api/Auth/login",{
+            await axios.post("https://localhost:7254/api/Auth/login", {
                 username: loginEmailRef.current.value,
                 password: loginPasswordRef.current.value,
-            }).then((response)=>{
+            }).then((response) => {
                 console.log(response.data);
                 toast.success("Login Successfully.");
-                localStorage.setItem("token",response.data['token']);
-                localStorage.setItem("role",response.data['role']);
-                localStorage.setItem("accNo",response.data['accNo']);
+                localStorage.setItem("token", response.data['token']);
+                localStorage.setItem("role", response.data['role']);
+                localStorage.setItem("accNo", response.data['accNo']);
 
             });
             // await login(loginEmailRef.current.value, loginPasswordRef.current.value)
             navigate("/home")
             //setUserName("");
             //setPassword("");
-        } catch(err) {
-            alert(err)
+        } catch (err) {
+            toast.error("Failed to log in.")
+            // alert(err)
         }
 
         setLoading(false)
     }
 
-    
+
 
     return (
         <div>
@@ -96,25 +98,25 @@ function Login() {
                                 <label>Password</label>
                             </div>
                             <div className="user-box">
-                                <input type="password" title="Confirm password" ref={passwordConfirmRef}  required />
+                                <input type="password" title="Confirm password" ref={passwordConfirmRef} required />
                                 <label>Confirm Password</label>
                             </div>
-                            
+
                             <div className="user-box">
                                 {/* <input type="text" title="Role" ref={role}  required /> */}
                                 {/* <label>Role</label> */}
                                 <select value={role}
-                                className="form-control1"
-                                id="Role"
-                                onChange={(event) => {
-                                    (setRole(event.target.value))
-                                }}>
-                                <option value="Admin"> Admin</option>
-                                <option value="User"> User</option>
+                                    className="form-control1"
+                                    id="Role"
+                                    onChange={(event) => {
+                                        (setRole(event.target.value))
+                                    }}>
+                                    <option value="Admin"> Admin</option>
+                                    <option value="User"> User</option>
                                 </select>
                             </div>
-                            {role=="User" &&  <div className="user-box">
-                                <input type="text" title="accNo"  onChange={(event) => {setaccNo(event.target.value);}} required />
+                            {role == "User" && <div className="user-box">
+                                <input type="text" title="accNo" onChange={(event) => { setaccNo(event.target.value); }} required />
                                 <label>AccNo</label>
                             </div>}
                             {/* <Link to="/home"> */}
@@ -127,11 +129,11 @@ function Login() {
                         <form onSubmit={handleLogin}>
                             <label for="chk" className="log">Login</label>
                             <div className="user-box">
-                                <input type="text" title="Enter Username" ref={loginEmailRef}  required />
+                                <input type="text" title="Enter Username" ref={loginEmailRef} required />
                                 <label>Username</label>
                             </div>
                             <div className="user-box">
-                                <input type="password" title="Enter password" ref={loginPasswordRef}  required />
+                                <input type="password" title="Enter password" ref={loginPasswordRef} required />
                                 <label>Password</label>
                             </div>
                             <button className="authBtn" disabled={loading} type="submit">Login</button>
--- src/index.js
+++ src/index.js
@@ -5,23 +5,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <ToastContainer
-position="top-right"
-autoClose={2000}
-hideProgressBar
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="colored"
-/>
+                position="top-right"
+                autoClose={2000}
+                hideProgressBar
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
   </React.StrictMode>
 );
 
